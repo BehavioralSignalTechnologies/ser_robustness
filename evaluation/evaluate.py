01e771d631dbb3802ee54ef7891b91c26353c9b3
Lefteris Kapelonis <lefteris@behavioralsignals.com>
2024-02-21 18:36:20 +0200

    Evaluate per session

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -6,21 +6,28 @@ def evaluate(preds, targets):
         targets: True labels: dictionary of {file_name: {"emotion": emotion, "fold": fold}}
 
     """
-    # TODO Average the metrics over the folds.
 
-    # Find the intersection of the keys
     if set(preds.keys()) != set(targets.keys()):
         raise ValueError("Predictions and targets are not aligned")
 
-    # Convert the dictionaries to lists
-    keys = list(preds.keys())
-    preds = [preds[key]["emotion"] for key in keys]
-    targets = [targets[key]["emotion"] for key in keys]
+    sessions = set([target["fold"] for target in targets.values()])
+    results = {}
+    for session in sessions:
+        keys = [key for key in targets.keys() if targets[key]["fold"] == session]
 
-    # Calculate the weighted accuracy
-    weighted_accuracy = (preds == targets).sum() / len(preds)
+        # Convert the dictionaries to lists
+        keys = list(preds.keys())
+        preds = [preds[key]["emotion"] for key in keys]
+        targets = [targets[key]["emotion"] for key in keys]
 
-    # Calculate the unweighted accuracy (average recall)
-    unweighted_accuracy = (preds == targets).sum(axis=0) / len(preds)
+        # Calculate the weighted accuracy
+        weighted_accuracy = (preds == targets).sum() / len(preds)
 
-    return weighted_accuracy, unweighted_accuracy
+        # Calculate the unweighted accuracy (average recall)
+        unweighted_accuracy = (preds == targets).sum(axis=0) / len(preds)
+
+        results[session] = [weighted_accuracy, unweighted_accuracy]
+
+    results["average"] = [sum([result[0] for result in results.values()]) / len(results),
+                          sum([result[1] for result in results.values()]) / len(results)]
+    return results
