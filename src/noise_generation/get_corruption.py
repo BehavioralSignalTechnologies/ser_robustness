35374328f66956daf22133a2ad63410dd8f83d2b
Lefteris Kapelonis <lefteris@behavioralsignals.com>
2024-02-23 18:05:22 +0200

    Add the gaussian corruption

--- src/noise_generation/get_corruption.py
+++ src/noise_generation/get_corruption.py
@@ -1,9 +1,11 @@
 from noise_generation.content import ContentCorruption
+from noise_generation.gaussian import AWGNAugmentation
 
 
 def get_corruption(corruption_name):
     if corruption_name == "content":
         return ContentCorruption
+    elif corruption_name == "gaussian":
+        return AWGNAugmentation
     else:
         raise ValueError(f"Unknown corruption: {corruption_name}")
-
