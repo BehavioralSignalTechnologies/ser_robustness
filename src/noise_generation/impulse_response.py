867c8d766add2be5415d221de3f6c1df4a486c65
apetrogianni <antonia@behavioralsignals.com>
2024-02-26 15:42:15 +0000

    Reverb

--- src/noise_generation/impulse_response.py
+++ src/noise_generation/impulse_response.py
@@ -24,8 +24,8 @@ class AddImpulseResponse(NoiseGeneration):
         self.p_reverb = config["p_reverb"]
 
         if self.ir_path is None:
-            raise ValueError(f"The {self.ir_path} is not provided.
-                              Please provide the path to the impulse response files.")
+            raise ValueError(f"The {self.ir_path} is not provided."+
+                             "Please provide the path to the impulse response files.")
 
         if not os.path.exists(self.ir_path):
             raise ValueError(f"The {self.ir_path} does not exist.")
@@ -46,3 +46,39 @@ class AddImpulseResponse(NoiseGeneration):
         """
 
         return self.transform(audio_data, sample_rate), os.path.basename(self.ir_path)
+
+
+if __name__ == "__main__":
+    import os, shutil, librosa
+    from scipy.io import wavfile
+
+    iemocap_dir = "/home/ubuntu/ser_robustness/iemocap/Session5/sentences/wav/"
+
+    # find 3 random files from ieomcap_dir
+    def foo():
+        audio_files = []
+
+        for root, dirs, files in os.walk(iemocap_dir):
+            for file in files:
+                if file.endswith(".wav"):
+                    print(os.path.join(root, file))
+                    audio_files.append(os.path.join(root, file))
+                    if len(audio_files) == 5:
+                        return audio_files
+
+
+    audio_files = foo()
+
+    config = {
+        "ir_path": "/home/ubuntu/ser_robustness/EchoThiefImpulseResponseLibrary/Underground",
+        "p_reverb": 1.0
+    }
+
+    augmentation = AddImpulseResponse(config)
+
+    for audio_file in audio_files:
+        shutil.copy(audio_file, ".")
+        audio, sample_rate = librosa.load(audio_file, sr=None)
+        s_aug, _ = augmentation.run(audio, sample_rate)
+        basename = os.path.basename(audio_file)
+        wavfile.write(f"{os.path.basename(audio_file).replace('.wav', '_reverb.wav')}", sample_rate, s_aug)
