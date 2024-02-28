be9f6cb3a8c7b29b1fa049421e9aefc9d82fb914
apetrogianni <antonia@behavioralsignals.com>
2024-02-28 15:40:26 +0000

    Update base class

--- src/noise_generation/corruptions.py
+++ src/noise_generation/corruptions.py
@@ -1,16 +1,3 @@
-"""
-Creating a noisy dataset.
-
-There are four available augmentation methods:
-    * reverberation
-    * clipping
-    * gain
-
-Maintainer Antonia Petrogianni
-Copyright Behavioral Signals Technologies
-"""
-
-
 class NoiseGeneration:
     """
     NoiseGeneration base class
@@ -25,12 +12,13 @@ class NoiseGeneration:
         """
         self.config = config
 
-    def run(self, audio_data, sample_rate):
+    def run(self, audio_data, sample_rate, output_file_path=None):
         """
         Run the augmentation method
 
-        :param audio_data: numpy array with the audio data
+        :param audio_data: numpy array with the audio data or the path to the audio file
         :param sample_rate: the sample rate
+        :param output_file_path: the path to save the augmented audio data
         :return: tuple with the augmented audio data (numpy array) and the applied noise (or None)
         """
         raise NotImplementedError
