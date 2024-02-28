8fc5eb9e9d5f3a4b325a17117faed3d74bd28459
apetrogianni <antonia@behavioralsignals.com>
2024-02-28 11:44:55 +0000

    minor change

--- src/noise_generation/impulse_response.py
+++ src/noise_generation/impulse_response.py
@@ -1,4 +1,10 @@
-"""
+import os
+from corruptions import NoiseGeneration
+from audiomentations import ApplyImpulseResponse
+
+
+class AddImpulseResponse(NoiseGeneration):
+    """
     Convolve the audio with a randomly selected impulse response.
 
     config: 
@@ -6,13 +12,7 @@
         `p_reverb` (float): the probability of applying reverberation
 
     *download the echo thief impulse response dataset: http://www.echothief.com/downloads/
-"""
-import os
-from corruptions import NoiseGeneration
-from audiomentations import ApplyImpulseResponse
-
-
-class AddImpulseResponse(NoiseGeneration):
+    """
     def __init__(self, config):
         """
         Initialize the ImpulseResponse class
@@ -46,39 +46,3 @@ class AddImpulseResponse(NoiseGeneration):
         """
 
         return self.transform(audio_data, sample_rate), None
-
-
-if __name__ == "__main__":
-    import os, shutil, librosa
-    from scipy.io import wavfile
-
-    iemocap_dir = "/home/ubuntu/ser_robustness/iemocap/Session5/sentences/wav/"
-
-    # find 3 random files from ieomcap_dir
-    def foo():
-        audio_files = []
-
-        for root, dirs, files in os.walk(iemocap_dir):
-            for file in files:
-                if file.endswith(".wav"):
-                    print(os.path.join(root, file))
-                    audio_files.append(os.path.join(root, file))
-                    if len(audio_files) == 5:
-                        return audio_files
-
-
-    audio_files = foo()
-
-    config = {
-        "ir_path": "/home/ubuntu/ser_robustness/EchoThiefImpulseResponseLibrary/Underground",
-        "p_reverb": 1.0
-    }
-
-    augmentation = AddImpulseResponse(config)
-
-    for audio_file in audio_files:
-        shutil.copy(audio_file, ".")
-        audio, sample_rate = librosa.load(audio_file, sr=None)
-        s_aug, _ = augmentation.run(audio, sample_rate)
-        basename = os.path.basename(audio_file)
-        wavfile.write(f"{os.path.basename(audio_file).replace('.wav', '_reverb.wav')}", sample_rate, s_aug)
